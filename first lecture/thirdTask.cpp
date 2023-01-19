0d2e8718f96e83c0f00d382e393926be7c196525
Valeri <valeri.t.yanev@gmail.com>
2023-01-19 12:29:16 +0200

    formatted for better visuallity

--- first lecture/thirdTask.cpp
+++ first lecture/thirdTask.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
     cout << "auto double break case enum char extern const float continue for default goto do if int long register return short signed sizeof static struct switch typedef union unsigned void volatile while asm bool catch class const_cast delete dynamic_cast explicit false friend inline mutable namespace new operator private protected public reinterpret_ cast static_cast template this throw true try typeid typename using virtual wchar_t";
 }
