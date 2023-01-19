a3dc3349de5438c142cb8a0dcb9ab95c5f521c12
Valeri <valeri.t.yanev@gmail.com>
2023-01-19 12:40:57 +0200

    write down on the console what are tokens, literals and commands

---
+++ first lecture/fourthTask.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Tokens:\nkey words ID's integers constants strings operators delimeters.";
+    cout << "\nLiteral: they are fixed values which can be integers, floating integers, string literals and character literals.";
+    cout << "\nCommands: they are not part of the program, they just control it's workflow; they compile, connect and replace text";
+}
