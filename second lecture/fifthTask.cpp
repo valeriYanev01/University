e4326843d491eeea478bf6a1878edab8d067cb8f
valeriYanev01 <valeri.t.yanev@gmail.com>
2023-01-26 12:45:48 +0200

    find simple price

---
+++ second lecture/fifthTask.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float bonboni = 3.5;
+    float bonboniPrice = 20;
+
+    float oneKilo = bonboniPrice / bonboni;
+    cout << oneKilo;
+}
