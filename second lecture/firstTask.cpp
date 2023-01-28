26a99a5692967a18105e5390ec9dcec6fdeb6801
valeriYanev01 <valeri.t.yanev@gmail.com>
2023-01-28 19:08:46 +0200

    enter data types from the console and output them with description

---
+++ second lecture/firstTask.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int integer;
+    char character;
+    float complexNumber;
+    string text;
+
+    cout << "Enter \"int\" data type: ";
+    cin >> integer;
+    cout << "Enter \"char\" data type: ";
+    cin >> character;
+    cout << "Enter \"float\" data type: ";
+    cin >> complexNumber;
+    cout << "Enter \"string\" data type: ";
+    cin >> text;
+
+    cout << integer;
+    cout << character;
+    cout << complexNumber;
+    cout << text;
+}
