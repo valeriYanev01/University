4499d04bd9ba47082b3fb110d08fea67c17c7ede
valeriYanev01 <valeri.t.yanev@gmail.com>
2023-02-05 14:21:43 +0200

    re-write the program with do while loop

--- fourthLecture/firstTask.cpp
+++ fourthLecture/firstTask.cpp
@@ -1,16 +1,34 @@
 #include <iostream>
 using namespace std;
 
+//---USING WHILE LOOP---
+// int main()
+// {
+//     int counter = 1;
+//     int number;
+//     cout << "Enter a number large than 1: ";
+//     cin >> number;
+
+//     while (counter <= number)
+//     {
+//         cout << counter << endl;
+//         counter++;
+//     }
+// }
+
+// _____________________________________
+// USING DO WHILE LOOP
+
 int main()
 {
-    int counter = 1;
-    int number;
-    cout << "Enter a number large than 1: ";
+    int number, counter = 1;
+
+    cout << "Enter a number larger than 1: ";
     cin >> number;
 
-    while (counter <= number)
+    do
     {
         cout << counter << endl;
         counter++;
-    }
+    } while (counter <= number);
 }
